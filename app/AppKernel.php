1290c9d227cee304129098b496b1fc34e3a8356f
Kristian Valentin <valentin.kristian@gmail.com>
2013-08-04 19:16:24 +0200

    upgrade to Symfony 2.3

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,11 +16,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new JMS\AopBundle\JMSAopBundle(),
-            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
-            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+
             new FinishmeBundle\FinishmeBundle(),
-        	new Braincrafted\BootstrapBundle\BraincraftedBootstrapBundle() // Twitter Bootstrap
+            new Bc\Bundle\BootstrapBundle\BcBootstrapBundle() // Twitter Bootstrap
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
